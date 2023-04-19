708f871b8b07a4b1d5fccc92f6551957627b9592
Revelation <revnyirongo@live.com>
2023-04-19 13:12:41 +0200

    Update openboxes-run.sh

--- openboxes-run.sh
+++ openboxes-run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set the path to the MySQL socket file
+export MYSQL_SOCKET=/var/run/mysqld/mysqld.sock
+
+# Set the JAVA_HOME environment variable
+export JAVA_HOME=/usr/lib/jvm/zulu-8-amd64
+
 # grails doesn't run without it for some reason
 export GRAILS_OPTS="-XX:-UseSplitVerifier -Xverify:none"
 
@@ -8,15 +14,17 @@ if [ ! -f /app/web-app/WEB-INF/applicationContext.xml ]; then
    grails upgrade --force --stacktrace
 fi
 
+# Install Node.js and npm
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
 
 npm cache clean --force
-npm cache verify
 npm i -g npm@6.14.6
 npm config set engine-strict true
-# npm update
 
-# some dependencies don't get resolved on thier own
-npm i --no-package-lock --legacy-peer-deps --force --verbose --stacktrace
+# some dependencies don't get resolved on their own
+npm i --no-package-lock --legacy-peer-deps --force --verbose
+
 grails run-app --stacktrace
 
 # if you want to compile enable this and stop grails run-app
